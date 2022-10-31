80771bf60726197fdd449faa70983a12bb073764
ThucNu <thucnu0608@gmail.com>
2022-10-31 11:22:58 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5500;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("uploads"));
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+}).then(()=>console.log("Connected to the database!"))
+.catch((error)=>console.log(error))
+
+app.listen(port,()=>console.log(`Server is running on port ${port}.`))
